6481c651eefe2f6e80b1b3add032c35937b982da
Evan Xd <itoyxd@gmail.com>
2015-06-12 09:05:22 +0800

    Can set one more pins at one time

--- js/ble_firmata.js
+++ js/ble_firmata.js
@@ -4,14 +4,18 @@
 (function(exports) {
   function BLEFirmata(options) {
     this.ble = new BluetoothHelper(options);
+    this._digitalPins = [];
   }
 
   BLEFirmata.DIGITAL_WRITE = 0x90;
   BLEFirmata.PIN_MODE = 0xF4;
 
   BLEFirmata.prototype = {
+    _digitalPins: null,
+
     digitalWrite: function(pin, value) {
       var data;
+      var pinData;
       var ble = this.ble;
       // Set pin mode.
       data = BLEFirmata.PIN_MODE.toString(16) +
@@ -20,7 +24,13 @@
       // Write digital data
       data = BLEFirmata.DIGITAL_WRITE.toString(16);
       if (pin > 1 && pin < 7) {
-        data += ble.paddingLeft((1 << pin).toString(16), 2) + '00';
+        pinData = 0;
+        this._digitalPins.forEach(function(value, pin) {
+          if (pin < 7 && value) {
+            pinData += 1 << pin;
+          }
+        });
+        data += ble.paddingLeft(pinData.toString(16), 2) + '00';
       } else if (pin === 7) {
         data += '00' + ble.paddingLeft(value.toString(16), 2);
       } else {
@@ -28,6 +38,7 @@
         return;
       }
       ble.sendData(data);
+      this._digitalPins[pin] = value;
     }
   };
 
