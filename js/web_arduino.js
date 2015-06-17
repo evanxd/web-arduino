ce0f0ac89932bb6b5cc5e6703f5c6b75370a3ef7
Evan Xd <itoyxd@gmail.com>
2015-06-17 09:41:06 +0800

    Fix bugs: create the web-arduino tag correctly

--- js/web_arduino.js
+++ js/web_arduino.js
@@ -4,24 +4,15 @@
 (function(window, document) {
   function WebArduino() {}
 
-  WebArduino.prototype = extend(Arduino.prototype, HTMLElement.prototype);
+  WebArduino.prototype = Object.create(HTMLElement.prototype);
 
   WebArduino.prototype.createdCallback = function() {
     console.log('The web-arduino tag is created.');
   };
 
-  function extend() {
-    var object = {};
-    for (var i = 0; i < arguments.length; i++) {
-      var attribute;
-      for (attribute in arguments[i]) {
-        object[attribute] = arguments[i][attribute];
-      }
-    }
-    return object;
-  }
-
   document.register('web-arduino', {
     prototype: WebArduino.prototype
   });
+  // For testing.
+  window.WebArduino = WebArduino;
 }(window, document));
